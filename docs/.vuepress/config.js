a570c751054bac4ec9fded5da622b614d349fe46
Wilfried Ifland <wilfried.ifland@gmail.com>
2018-06-25 17:09:03 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  title: 'Netlify CMS + VuePress',
+  description: 'Netlify + VuePress',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'iwilfried/vuepress-boilerplate'
+  }
+}
