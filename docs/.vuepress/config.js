35d4e32b5b5d35fda165a1ad973872d9debb4180
Wilfried Ifland <wilfried.ifland@gmail.com>
2018-07-18 11:04:07 +0200

    toc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,14 @@ module.exports = {
   title: 'VuePress ',
   description: 'VuePress Project Documentation System',
 
-    
-
  markdown: {
     // options for markdown-it-anchor
     
-  // options for markdown-it-anchor
-    anchor: { permalink: false },
-    // options for markdown-it-toc
-    toc: { includeLevel: [1, 2] },
+  
 	
     config: md => {
       // use more markdown-it plugins!
-      md.use(require("markdown-it-table-of-contents"), [1,2]);
+	  md.use(require("markdown-it-table-of-contents"), { includeLevel: [1, 2] }); 
     }
   }
 }
